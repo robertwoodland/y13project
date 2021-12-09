f31199419440647d74afdeaad2ed5eb507c98579
Robert Woodland <robertwoodland47@gmail.com>
2021-12-09 12:23:13 +0000

    Add firebase creds

--- src/components/base.js
+++ src/components/base.js
@@ -3,7 +3,13 @@ import "firebase/auth";
 import "firebase/firestore"
 
 const app = firebase.initializeApp({
-    //Insert credentials here
+    apiKey: "AIzaSyDcjFqBEiN6xTa0mYdjOkifoqh24NuPDKI",
+    authDomain: "y13project-9c2a6.firebaseapp.com",
+    projectId: "y13project-9c2a6",
+    storageBucket: "y13project-9c2a6.appspot.com",
+    messagingSenderId: "319905312780",
+    appId: "1:319905312780:web:9f39b9214e19bcde0abe29",
+    measurementId: "${config.measurementId}"
 });
 
 export default app
