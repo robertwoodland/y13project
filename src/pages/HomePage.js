ad14cdcf4af82434d3dce633e4017a84790050f8
Robert Woodland <robertwoodland47@gmail.com>
2021-12-31 16:00:55 +0000

    Install Bootstrap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {Button} from 'react-bootstrap'
 
 export default function HomePage() {
     return (
         <div>
             <div class="bg-purple-300 h-screen flex items-center justify-center">
-            <div class="bg-white border border-gray-400 shadow-lg flex flex-col items-center justify-center w-11/12 h-5/6 rounded-md">
-            <h1>
-                This is the Home Page
-            </h1>
-            <Link to='/logout'>Log Out</Link>
-            </div>
+                <div class="bg-white border border-gray-400 shadow-lg flex flex-col items-center justify-center w-11/12 h-5/6 rounded-md">
+                    <h1>
+                        This is the Home Page
+                    </h1>
+                    <Link to='/logout'>
+                        <Button variant='primary'>Log Out</Button>
+                    </Link>
+                </div>
 
-        </div>
+            </div>
         </div>
     )
 }
+
+
+
