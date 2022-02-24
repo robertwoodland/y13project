adb7a5c396499239ebc4311370cd5a374838a786
Robert Woodland <robertwoodland47@gmail.com>
2022-02-24 19:26:31 +0000

    Bug Fix

--- src/components/Firebase Functions/updatePreferences.js
+++ src/components/Firebase Functions/updatePreferences.js
@@ -2,9 +2,11 @@ import React from "react";
 import app from "../base";
 
 export default function updatePreferences(username, primaryColour, secondaryColour, uid){
-    app.firestore().collection('users').doc(uid).update({
-        username: username,
-        primaryColour: primaryColour,
-        secondaryColour: secondaryColour
-    })
+    if (username && primaryColour && secondaryColour) {
+        app.firestore().collection('users').doc(uid).update({
+            username: username,
+            primaryColour: primaryColour,
+            secondaryColour: secondaryColour
+        })
+    }
 }
